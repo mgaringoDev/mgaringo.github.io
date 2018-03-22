6917a50712bae5177971d56ef6996d17b05a6597
mgaringoDev <mg.mydev@gmail.com>
2018-03-22 16:15:01 -0400

    Modal Scripting Working
    Added the python scripting for the modals for the cards

--- python/mainCreateProjectCards.py
+++ python/mainCreateProjectCards.py
@@ -6,7 +6,6 @@ Created on Thu Mar 22 11:35:09 2018
 """
 
 import pandas as pd
-import numpy as np
 
 # Read the data
 dataFrame = pd.read_csv("projectsDB.csv");
@@ -16,100 +15,83 @@ numberOfRows = dataFrame.shape[0];
 numberOfColoumns = dataFrame.shape[1];
 
 #%%
-fileName = 'testfile.txt';
-file = open(fileName,"w+");
+fileNameCard = 'card.txt';
+fileCard = open(fileNameCard,"w+");
 
-currentCategory = 'Nothing'
-currentProjectID = 'Nothing'
+fileNameModal = 'modal.txt';
+fileModal = open(fileNameModal,"w+");
 
-for x in xrange(numberOfRows):
-    if(currentCategory != dataFrame['Category'][x]):
-        file.write('<!-- ----------------------- %s Projects ------------------- -->\n' % dataFrame['Category'][x]);
+
+currentCategoryCard = 'Nothing'
+currentProjectIDCard = 'Nothing'
+
+currentCategoryModal = 'Nothing'
+currentProjectIDModal = 'Nothing'
+
+for x in xrange(numberOfRows):    
+    
+    #% --------------------- This portion writes to the modal text file ---------------------------------------------------------
+    if(currentCategoryModal != dataFrame['Category'][x]):
+        fileModal.write('<!-- ----------------------- %s Projects Modals------------------- -->\n' % dataFrame['Category'][x]);
         currentCategory = dataFrame['Category'][x]
-    file.write('\t<!-- ----------------------- %s ------------------- -->\n' % dataFrame['ProjectID'][x]);
-    htmlString = '<div class=\"portfolio-item col-xs-12 col-sm-4 col-md-3\" data-groups=\''+dataFrame['Tags'][x]+'\' style=\"color:blue;\">' \
-                   '\t<article class=\"blog-post-wrapper\" >' \
-                      '<div class=\"figure\">'\
-                         '<div class=\"post-thumbnail\">' \
-                           '<a href=\"#\" data-toggle=\"modal\" data-target=\"#'+dataFrame['ProjectID'][x]+'\"><img src=\"'+dataFrame['ImageLocation'][x]+'" class=\"img-responsive \" alt=\"\"></a>' \
-                         '</div>' \
-                         '<i class=\"fa fa-file-photo-o\"></i>' \
-                      '</div>' \
-                      '<!-- /.figure -->' \
-                      '<header class=\"entry-header\">' \
-                         '<div class=\"post-meta\">' \
-                            '<span class=\"the-category\"> ' \
-                            '<a >'+dataFrame['Year'][x]+'</a>' \
-                            '</span>' \
-                         '</div>' \
-                         '<h2 class=\"entry-title\"><a href=\"#\" data-toggle=\"modal\" data-target=\"#'+dataFrame['ProjectID'][x]+'\">'+dataFrame['Title'][x]+'</a></h2>' \
-                      '</header>' \
-                      '<!-- .entry-header -->' \
-                   '</article>' \
-                   '<!-- /.col-sm-4 -->' \
+    fileModal.write('\t<!-- ----------------------- %s ------------------- -->\n' % dataFrame['ProjectID'][x]);
+    htmlModalString = '<div class="modal fade" id="'+dataFrame['ProjectID'][x]+'" role="dialog">' \
+                '<div class="modal-dialog modal-lg">' \
+                    '<div class="modal-content">' \
+                        '<div class="modal-header">' \
+                            '<button type="button" class="close" data-dismiss="modal">&times;</button>' \
+                            '<h4 class="modal-title">Description</h4>' \
+                        '</div>' \
+                        '<div class="modal-body">' \
+                            '<p>'+dataFrame['Description'][x]+'</p>' \
+                        '</div>' \
+                        '<div class="modal-footer">' \
+                            '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' \
+                        '</div>' \
+                    '</div>' \
                 '</div>' \
-                '<!-- /.portfolio-item -->' \
-                '\n'
-    file.write(htmlString);
+            '</div>'\
+            '\n'
+    fileModal.write(htmlModalString);
+    
+    
+    
+#    #% --------------------- This portion writes to the card text file ---------------------------------------------------------
+#    if(currentCategoryCard != dataFrame['Category'][x]):
+#        fileCard.write('<!-- ----------------------- %s Projects ------------------- -->\n' % dataFrame['Category'][x]);
+#        currentCategoryCard = dataFrame['Category'][x]
+#    fileCard.write('\t<!-- ----------------------- %s ------------------- -->\n' % dataFrame['ProjectID'][x]);
+#    htmlString = '<div class=\"portfolio-item col-xs-12 col-sm-4 col-md-3\" data-groups=\''+dataFrame['Tags'][x]+'\' style=\"color:blue;\">' \
+#                   '\t<article class=\"blog-post-wrapper\" >' \
+#                      '<div class=\"figure\">'\
+#                         '<div class=\"post-thumbnail\">' \
+#                           '<a href=\"#\" data-toggle=\"modal\" data-target=\"#'+dataFrame['ProjectID'][x]+'\"><img src=\"'+dataFrame['ImageLocation'][x]+'" class=\"img-responsive \" alt=\"\"></a>' \
+#                         '</div>' \
+#                         '<i class=\"fa fa-file-photo-o\"></i>' \
+#                      '</div>' \
+#                      '<!-- /.figure -->' \
+#                      '<header class=\"entry-header\">' \
+#                         '<div class=\"post-meta\">' \
+#                            '<span class=\"the-category\"> ' \
+#                            '<a >'+dataFrame['Year'][x]+'</a>' \
+#                            '</span>' \
+#                         '</div>' \
+#                         '<h2 class=\"entry-title\"><a href=\"#\" data-toggle=\"modal\" data-target=\"#'+dataFrame['ProjectID'][x]+'\">'+dataFrame['Title'][x]+'</a></h2>' \
+#                      '</header>' \
+#                      '<!-- .entry-header -->' \
+#                   '</article>' \
+#                   '<!-- /.col-sm-4 -->' \
+#                '</div>' \
+#                '<!-- /.portfolio-item -->' \
+#                '\n'
+#    fileCard.write(htmlString);
+    
+    
 
  
-file.close() 
+fileCard.close() 
+fileModal.close()
 
 #%%
-test = 'WHAAAAA'
-longStr = 'This is a very long %s ' \
-        'that I wrote to help %s ' \
-        'who had a question about ' \
-        'writing long strings in Python' \
-        % (test, test)
-        
-longStr
 
-#%%
-htmlString = '<div class=\"portfolio-item col-xs-12 col-sm-4 col-md-3\" data-groups=\''+dataFrame['Tags'][x]+'\' style=\"color:blue;\">' \
-               '\t<article class=\"blog-post-wrapper\" >' \
-                  '<div class=\"figure\">'\
-                     '<div class=\"post-thumbnail\">' \
-                       '<a href=\"#\" data-toggle=\"modal\" data-target=\"#'+dataFrame['ProjectID'][x]+'\"><img src=\"'+dataFrame['ImageLocation'][x]+'" class=\"img-responsive \" alt=\"\"></a>' \
-                     '</div>' \
-                     '<i class=\"fa fa-file-photo-o\"></i>' \
-                  '</div>' \
-                  '<!-- /.figure -->' \
-                  '<header class=\"entry-header\">' \
-                     '<div class=\"post-meta\">' \
-                        '<span class=\"the-category\"> ' \
-                        '<a >'+dataFrame['Year'][x]+'</a>' \
-                        '</span>' \
-                     '</div>' \
-                     '<h2 class=\"entry-title\"><a href=\"#\" data-toggle=\"modal\" data-target=\"#'+dataFrame['ProjectID'][x]+'\">'+dataFrame['Title'][x]+'</a></h2>' \
-                  '</header>' \
-                  '<!-- .entry-header -->' \
-               '</article>' \
-               '<!-- /.col-sm-4 -->' \
-            '</div>' \
-            '<!-- /.portfolio-item -->' \
-#%%
-            htmlString = '<div class=\"portfolio-item col-xs-12 col-sm-4 col-md-3\" data-groups=\'%s\' style=\"color:blue;\">' \
-               '\t<article class=\"blog-post-wrapper\" >' \
-                  '<div class=\"figure\">'\
-                     '<div class=\"post-thumbnail\">' \
-                       '<a href=\"#\" data-toggle=\"modal\" data-target=\"#%s\"><img src=\"%s" class=\"img-responsive \" alt=\"\"></a>' \
-                     '</div>' \
-                     '<i class=\"fa fa-file-photo-o\"></i>' \
-                  '</div>' \
-                  '<!-- /.figure -->' \
-                  '<header class=\"entry-header\">' \
-                     '<div class=\"post-meta\">' \
-                        '<span class=\"the-category\"> ' \
-                        '<a >%s</a>' \
-                        '</span>' \
-                     '</div>' \
-                     '<h2 class=\"entry-title\"><a href=\"#\" data-toggle=\"modal\" data-target=\"#%s\">%s</a></h2>' \
-                  '</header>' \
-                  '<!-- .entry-header -->' \
-               '</article>' \
-               '<!-- /.col-sm-4 -->' \
-            '</div>' \
-            '<!-- /.portfolio-item -->' \
-            % (dataFrame['Tags'][x],dataFrame['ProjectID'][x],dataFrame['ImageLocation'][x],dataFrame['Year'][x],dataFrame['ProjectID'][x],dataFrame['Title'][x])        
             
